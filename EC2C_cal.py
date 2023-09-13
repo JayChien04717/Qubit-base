62628b4aabc5a7d6e738aad11c6cb3591eb0d4a5
Jay Chien <zx123c123@gmail.com>
2023-09-13 11:05:02 +0800

    Ec calculator

--- EC2C_cal.py
+++ EC2C_cal.py
@@ -1,15 +1,20 @@
 import numpy as np
+
+h = 6.62607015e-34
+q = 1.602e-19
+
 def EC_cal():
-    h = 6.62607015e-34
-    q = 1.602e-19
     EC = float(input("inpurt the EC frequency(Hz):"))
     C = q**2/(2*EC*h)
     print(f"capacitor is {C*1e15:.2f} fF") 
 
 def EL_cal():
-    h = 6.62607015e-34
-    q = 1.602e-19
     EL = float(input("inpurt the EL frequency(Hz):"))
     L = np.pi**2*h/(EL*q**2)
-    print(f"capacitor is {L*1e6:.2f} nH") 
+    print(f"inductance is {L*1e6:.2f} nH") 
 
+def capacitance_cal(target_freq):
+    z_im = float(input("inpurt the imaginary part of impencdence:"))
+    capacitor = 1/(2*np.pi*z_im*target_freq)
+    print(f"capacitor is {capacitor*1e15:.2f} fF")
+    
